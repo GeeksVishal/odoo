2275031683ecf8934f2f6fdb58ec42b7c9f8c39b
umang41 <chauhanumang639@gmail.com>
2025-07-12 10:06:28 +0530

    signup commited

---
+++ lib/screens/signup_screen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:odoo/screens//Login_screen.dart';
+
+class SignupScreen extends StatelessWidget {
+  SignupScreen({super.key});
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:
+      Center(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.lock_outline,
+                size: 100,
+                color: Colors.blue,
+              ),
+              SizedBox(height: 40),
+              TextField(
+                controller: _emailController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.email),
+                ),
+              ),
+              SizedBox(height: 20),
+              TextField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.lock),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text("Forgot Password")
+                ],
+              ),
+              SizedBox(height: 30),
+              ElevatedButton(
+                  child: Text('Login'),
+                  onPressed: () {
+                    Navigator.pushReplacementNamed(context, '/login');
+                  }
+
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
