12a397e6d6650981e2e01912cf067d49ff9177d6
GeeksVishal <vishalkukadiya2262@gmail.com>
2025-07-12 10:25:34 +0530

    forget screen commitedx

---
+++ lib/screens/forget_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class ForgetScreen extends StatelessWidget {
+  final TextEditingController controller=TextEditingController();
+
+  const ForgetScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TextField(
+          controller: controller,
+          decoration: const InputDecoration(
+            labelText: 'Enter Current Password',
+            border: OutlineInputBorder(),
+          ),
+        ),
+        const SizedBox(height: 16),
+        ElevatedButton(
+          onPressed: () {
+            
+          },
+          child: const Text('Reset'),
+        ),
+      ],
+    );
+  }
+}
