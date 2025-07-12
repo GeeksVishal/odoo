6bf8a749b2cc2e98845c0b9c07fae4f28123af7e
Dev241007 <jethavadevendra24@gmail.com>
2025-07-12 10:54:09 +0530

    Merge remote-tracking branch 'origin/master'

--- lib/screens/forget_screen.dart
+++ lib/screens/forget_screen.dart
@@ -8,23 +8,39 @@ class ForgetScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-        children: [
-          TextField(
-            controller: controller,
-            decoration: const InputDecoration(
-              labelText: 'Enter Current Password',
-              border: OutlineInputBorder(),
-            ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.lock_outline, size: 100, color: Colors.blue),
+              SizedBox(height: 40),
+              TextField(
+                controller: controller,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.email),
+                ),
+              ),
+              SizedBox(height: 15,),
+              Container(
+                width: 180,
+                height: 40,
+                child: OutlinedButton(
+                  onPressed: () {},
+                  style: OutlinedButton.styleFrom(
+                    foregroundColor: Colors.white, // Text color// Border color
+                    backgroundColor: Colors.blue, // Background color (optional)
+                  ),
+                  child: Text('Forget Password'),
+                ),
+              ),
+            ],
           ),
-          const SizedBox(height: 16),
-          ElevatedButton(
-            onPressed: () {
-              
-            },
-            child: const Text('Reset'),
-          ),
-        ],
+        ),
       ),
     );
   }
