8c295aa93086655d6ad051ec234d3a29f30ffa07
GeeksVishal <vishalkukadiya2262@gmail.com>
2025-07-12 09:54:47 +0530

    login screen commited

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,28 +1,65 @@
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
-  TextEditingController 
-  const LoginScreen({super.key});
+  LoginScreen({super.key});
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+   
+   void _Login(){
+
+   }
+  
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-                Icon(
-                  Icons.home,
-                  size: 100,
-                  color: Colors.blue,
-                  ),
-                  SizedBox(height: 40,),
-                  TextField(
-                    
-                  )
+              Icon(
+                Icons.lock_outline,
+                size: 100,
+                color: Colors.blue,
+              ),
+              SizedBox(height: 40),
+              TextField(
+                controller: _emailController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.email),
+                ),
+              ),
+              SizedBox(height: 20),
+              TextField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.lock),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text("Forgot Password")
+                ],
+              ),
+              SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () => _Login(),
+                child: Text('Login'),
+              ),
             ],
           ),
         ),
-      );
+      ),
+    );
   }
 }
